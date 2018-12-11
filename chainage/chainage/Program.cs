cfc51578c43cad8b8f521f3bbb3c1dd1fef8473b
En_Tech_Siast <jg_972@hotmail.com>
2018-12-11 14:01:04 +0100

    init

---
+++ chainage/chainage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chainage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
