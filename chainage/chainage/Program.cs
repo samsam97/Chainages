d0263f7be83a7a4fe3c2af546631bb99eab7d553
En_Tech_Siast <jg_972@hotmail.com>
2018-12-11 14:50:33 +0100

    wip

--- chainage/chainage/Program.cs
+++ chainage/chainage/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace chainage
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] regle1 = new[] {"A","B","C"};
+            string[] regle2 = new[] {"I","H"};
+            string[] regle3 = new[] {"H","H"};
+            string[] regle4 = new[] {"A"};
+            string[] regle5 = new[] {"E","F"};
+            string[] regle6 = new[] {"A"};
+            string[] regle7 = new[] {"K","L"};
+            string[] regle8 = new[] {"A"};
+
+            Dictionary<string,string[]> baseDeRegle = new Dictionary<string, string[]>();
+
+            foreach (KeyValuePair<string, string[]> kvp in baseDeRegle)
+            {
+                if(kvp.Value.Contains(kvp.Key))
+                
+            }
+            
+        }
+
+        public bool checkIfKeyIsInArray(KeyValuePair key, string[] array)
+        {
+            var result = false;
+            foreach (var value in array)
+            {
+                if (value == key)
+                    result = true;
+            }
+            return result;
+
         }
     }
 }
+
